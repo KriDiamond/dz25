9898a30ddc723b85b6afe243a4937c7a14beddd6
Heather Diamond <baishenkokv@gmail.com>
2025-03-31 20:43:43 +0300

    свернула в генератор

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 def sum_of_lists (list1, list2):
-    if len(list1) != len(list2):
-        if len(list1) > len(list2):
-            list2 += [0] * (len(list1) - len(list2))
-        else:
-            list1 += [0] * (len(list) - len(list1))
-    return [x+y for x,y in zip(list1, list2)]
+    return [x + y for x, y in zip(list1, list2)] + list1[len(list2):] + list2[len(list1):]
 
 
 list1 = [int(i) for i in input(f"Введите через пробел числа первого списка: ").split()]
